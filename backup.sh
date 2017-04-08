7bfedabff070082e54cf72839a62788fc634f41b
Jan Wagner <waja@cyconet.org>
2017-04-08 16:09:59 +0200

    Remove backslash before ${TARGET}, it breaks cleanup
    Closes #1

--- backup.sh
+++ backup.sh
@@ -21,8 +21,8 @@ if [ -n "$MAX_BACKUPS" ]; then
   while [ `ls -1 /backup | wc -l` -gt "$MAX_BACKUPS" ];
   do
     TARGET=`ls -1 /backup | sort | head -n 1`
-    echo "Backup \${TARGET} is deleted"
-    rm -rf /backup/\${TARGET}
+    echo "Backup ${TARGET} is deleted"
+    rm -rf /backup/${TARGET}
   done
 fi
 

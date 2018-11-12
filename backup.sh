2b8cb54d0548bd9760e7b006ff9c5229a3cbab33
Manuel <mmartinortiz@gmail.com>
2018-11-12 21:12:53 +0100

    Increased verbosity

--- backup.sh
+++ backup.sh
@@ -3,18 +3,19 @@
 [ -z "${MYSQL_PASS:=$MYSQL_PASSWORD}" ] && { echo "=> MYSQL_PASS cannot be empty" && exit 1; }
 
 DATE=$(date +%Y%m%d%H%M)
-echo "=> Backup started at $DATE"
+echo "=> Backup started at $(date "+%Y-%m-%d %H:%M:%S")"
 databases=${MYSQL_DATABASE:-${MYSQL_DB:-$(mysql -h "$MYSQL_HOST" -P "$MYSQL_PORT" -u "$MYSQL_USER" -p"$MYSQL_PASS" -e "SHOW DATABASES;" | tr -d "| " | grep -v Database)}}
 for db in $databases
 do
   if [[ "$db" != "information_schema" ]] && [[ "$db" != "performance_schema" ]] && [[ "$db" != "mysql" ]] && [[ "$db" != _* ]]
   then
-    echo "Dumping database: $db"
+    echo "==> Dumping database: $db"
     FILENAME=/backup/$DATE.$db.sql
     LATEST=/backup/latest.$db.sql.gz
-    if mysqldump -h "$MYSQL_HOST" -P "$MYSQL_PORT" -u "$MYSQL_USER" -p"$MYSQL_PASS" --databases "$db" "$MYSQLDUMP_OPTS" > "$FILENAME"
+    if mysqldump -h "$MYSQL_HOST" -P "$MYSQL_PORT" -u "$MYSQL_USER" -p"$MYSQL_PASS" --databases "$db" $MYSQLDUMP_OPTS > "$FILENAME"
     then
       gzip -f "$FILENAME"
+      echo "==> Creating symlink to latest backup: $(basename "$FILENAME".gz)"
       rm "$LATEST" 2> /dev/null
       cd backup && ln -s $(basename "$FILENAME".gz) $(basename "$LATEST") && cd ..
     else
@@ -25,12 +26,13 @@ done
 
 if [ -n "$MAX_BACKUPS" ]
 then
+  echo "=> Max number of backups ("$MAX_BACKUPS") reached. Deleting oldest backups"
   while [ "$(find /backup -maxdepth 1 -name "*.sql.gz" -type f | wc -l)" -gt "$MAX_BACKUPS" ]
   do
     TARGET=$(find /backup -maxdepth 1 -name "*.sql.gz" -type f | sort | head -n 1)
-    echo "Backup $TARGET is deleted"
     rm -rf "$TARGET"
+    echo "==> Backup $TARGET has been deleted"
   done
 fi
 
-echo "=> Backup done"
+echo "=> Backup process finished at echo $(date "+%Y-%m-%d %H:%M:%S")"
